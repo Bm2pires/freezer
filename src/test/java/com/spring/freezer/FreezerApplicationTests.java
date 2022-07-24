d6cc1d247278723c4e96affd41f1b81419c9deb3
Bredan Pires <piresbredan@gmail.com>
2022-07-24 12:53:57 +0100

    initial commit

---
+++ src/test/java/com/spring/freezer/FreezerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.freezer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreezerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
